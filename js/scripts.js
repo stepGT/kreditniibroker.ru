ecab2d09f602c2909f6ce30321f340e706f27a7f
stepGT <stepgt@mail.ru>
2019-12-23 10:37:06 +1000

    Added slick

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+  $('.tariff__slick').slick({
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+});
