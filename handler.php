11442173b78adb627f85b17e669242da49143df5
Andrey <stepgt@mail.ru>
2020-01-09 12:59:29 +1000

    Fix

--- handler.php
+++ handler.php
@@ -1,19 +1,17 @@
 <?php
 $to = "stepgt@mail.ru";
 $subject = "Заявку на бесплатную консультацию";
-//$subject = "This is subject";
-
-$message = "<p>Имя:$_POST[name]</p>";
-$message .= "<p>Телефон: $_POST[phone]</p>";
-$message .= "<p>Сумма: . $_POST[summ]</p>";
-$message .= "<p>Срок: . $_POST[time]</p>";
-
-
+//
+$message = "<p>Имя: $_POST[name]</p>";
+$message .= "<p>Телефон: +7$_POST[phone]</p>";
+$message .= "<p>Сумма: $_POST[summ] руб.</p>";
+$message .= "<p>Срок: $_POST[time] мес.</p>";
+//
 $header = "From:abc@somedomain.com \r\n";
 $header .= "Cc:stepgs@yandex.ru \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
-
+//
 $retval = mail($to, $subject, $message, $header);
 
 if ($retval == TRUE) {
