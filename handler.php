f5a88b92a49eb416c97141c8b2389923310eafbc
Andrey <stepgt@mail.ru>
2020-01-09 13:02:50 +1000

    Change recipient

--- handler.php
+++ handler.php
@@ -1,5 +1,5 @@
 <?php
-$to = "stepgt@mail.ru";
+$to = "stepgt@mail.ru, 22012764@mail.ru";
 $subject = "Заявку на бесплатную консультацию";
 //
 $message = "<p>Имя: $_POST[name]</p>";
@@ -7,7 +7,7 @@ $message .= "<p>Телефон: +7$_POST[phone]</p>";
 $message .= "<p>Сумма: $_POST[summ] руб.</p>";
 $message .= "<p>Срок: $_POST[time] мес.</p>";
 //
-$header = "From:abc@somedomain.com \r\n";
+$header = "From:robot@kreditniibroker.ru \r\n";
 $header .= "Cc:stepgs@yandex.ru \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
@@ -15,9 +15,9 @@ $header .= "Content-type: text/html\r\n";
 $retval = mail($to, $subject, $message, $header);
 
 if ($retval == TRUE) {
-  echo "Message sent successfully...";
+  echo "Сообщение успешно отправлено...";
 }
 else {
-  echo "Message could not be sent...";
+  echo "Сообщение не может быть отправлено...";
 }
 ?>
