8fddd2fc2f28112a260b28ada04e6172d6886337
Andrey <stepgt@mail.ru>
2020-01-09 12:54:10 +1000

    Correct handler

--- handler.php
+++ handler.php
@@ -1,14 +1,15 @@
 <?php
 $to = "stepgt@mail.ru";
-$subject = "Заявку на бесплатную консультацию";
+//$subject = "Заявку на бесплатную консультацию";
+$subject = "This is subject";
 
-$message = "<p>Имя:$_POST[name]</p>";
-$message .= "<p>Телефон: $_POST[phone]</p>";
-$message .= "<p>Сумма: ' . $_POST[summ]</p>";
-$message .= "<p>Срок: ' . $_POST[time]</p>";
+$message = "<p>NAME:$_POST[name]</p>";
+$message .= "<p>PHONE: $_POST[phone]</p>";
+$message .= "<p>SUMM: ' . $_POST[summ]</p>";
+$message .= "<p>TIME: ' . $_POST[time]</p>";
 
 
-$header = "From:kreditniibroker.ru \r\n";
+$header = "From:abc@somedomain.com \r\n";
 $header .= "Cc:stepgs@yandex.ru \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
@@ -16,9 +17,9 @@ $header .= "Content-type: text/html\r\n";
 $retval = mail($to, $subject, $message, $header);
 
 if ($retval == TRUE) {
-  echo "Спасибо! Ваша заявка была успешно отправлена!";
+  echo "Message sent successfully...";
 }
 else {
-  echo "Ошибка! Повторите попытку позже!";
+  echo "Message could not be sent...";
 }
 ?>
