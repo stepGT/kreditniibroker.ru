e0c89d3641644881ba608a66262ff25e7d9203b9
Andrey <stepgt@mail.ru>
2020-01-10 15:56:04 +1000

    Form validate input

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,19 @@ $(function () {
       }
     ]
   });
+  //
+  $('.header__form form input').keyup(function () {
+    var empty = false;
+    $('.header__form form').find('input[type="text"]').each(function () {
+      if (($(this).val() == '')) {
+        empty = true;
+      }
+    });
+    if (empty) {
+      $('.header__form button').css('background', '#ccc').attr('disabled', 'disabled');
+    }
+    else {
+      $('.header__form button').css('background', '#3363be').removeAttr('disabled');
+    }
+  });
 });
