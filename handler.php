09c5b38e232bdc10b18bc88d15f9453540f4fe87
Andrey <stepgt@mail.ru>
2020-01-28 17:47:49 +1000

    Added new email

--- handler.php
+++ handler.php
@@ -1,14 +1,13 @@
 <?php
-$to = "stepgt@mail.ru, 22012764@mail.ru";
-$subject = "Заявку на бесплатную консультацию";
+$to      = "stepgt@mail.ru, 22012764@mail.ru, 101@businessinvets24.ru";
 $subject = "Заявка на бесплатную консультацию kreditniibroker.ru";
 //
-$message = "<p>Имя: $_POST[name]</p>";
+$message  = "<p>Имя: $_POST[name]</p>";
 $message .= "<p>Телефон: $_POST[phone]</p>";
 $message .= "<p>Сумма: $_POST[summ] руб.</p>";
 $message .= "<p>Срок: $_POST[time] (лет)</p>";
 //
-$header = "From:robot@kreditniibroker.ru \r\n";
+$header  = "From:robot@kreditniibroker.ru \r\n";
 $header .= "Cc:stepgs@yandex.ru \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
