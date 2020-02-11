1c42bca06dd5dfc7537d6fdbb0bfbb3bb0e64785
Andrey <stepgt@mail.ru>
2020-02-11 12:40:07 +1000

    Removed unnecessary email addresses

--- handler.php
+++ handler.php
@@ -1,5 +1,5 @@
 <?php
-$to      = "stepgt@mail.ru, 22012764@mail.ru, 101@businessinvest24.ru";
+$to      = "22012764@mail.ru, 101@businessinvest24.ru";
 $subject = "Заявка на бесплатную консультацию kreditniibroker.ru";
 //
 $message  = "<p>Имя: $_POST[name]</p>";
@@ -8,7 +8,7 @@ $message .= "<p>Сумма: $_POST[summ] руб.</p>";
 $message .= "<p>Срок: $_POST[time] (лет)</p>";
 //
 $header  = "From:robot@kreditniibroker.ru \r\n";
-$header .= "Cc:stepgs@yandex.ru \r\n";
+$header .= "Cc:101@businessinvest24.ru \r\n";
 $header .= "MIME-Version: 1.0\r\n";
 $header .= "Content-type: text/html\r\n";
 //
